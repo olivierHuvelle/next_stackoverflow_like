618c417aab55707c9c5e1a59a506ccaafe19667f
Olivier Huvelle <olivier.huvelle@gmail.com>
2024-03-19 21:03:19 +0100

    theHeader component implemented but detected performance issues dued to cookie manipulation

--- app/components/layout/TheHeader.tsx
+++ app/components/layout/TheHeader.tsx
@@ -7,12 +7,49 @@ import {
     NavbarItem,
     Input,
     Button,
-    Avatar
+    Avatar,
+    Popover,
+    PopoverContent,
+    PopoverTrigger
 } from "@nextui-org/react";
 import paths from "@/utils/paths";
+import {signIn, signOut} from "@/actions/authentication";
 
 export default async function TheHeader(){
     const session = await auth()
+
+    const conditionalRendering = () => {
+        if(session?.user){
+            return (
+                <Popover placement="left">
+                    <PopoverTrigger>
+                        <Avatar src={session.user.image || ''} />
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <div className="p-4">
+                            <form action={signOut}>
+                                <Button type="submit" color="danger" variant="bordered">Sign Out</Button>
+                            </form>
+                        </div>
+                    </PopoverContent>
+                </Popover>
+            )
+        }
+        return (
+            <>
+                <NavbarItem>
+                    <form action={signIn}>
+                        <Button type="submit" color="primary" variant="bordered">Sign In</Button>
+                    </form>
+                </NavbarItem>
+                <NavbarItem>
+                    <form action={signIn}>
+                        <Button type="submit" color="success" variant="bordered">Sign up</Button>
+                    </form>
+                </NavbarItem>
+            </>
+        )
+    }
 
     return (
         <Navbar  className="shadow mb-6">
@@ -23,9 +60,7 @@ export default async function TheHeader(){
                 <Input />
             </NavbarContent>
             <NavbarContent justify="end">
-                <NavbarItem>
-                    <div>{session?.user ? 'signed in' : 'signed out'}</div>
-                </NavbarItem>
+                {conditionalRendering()}
             </NavbarContent>
         </Navbar>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,7 @@
-import {signIn, signOut} from "@/actions/authentication";
-import {auth} from "@/auth/auth";
-import {Button} from "@nextui-org/react";
-
 export default async function Home() {
-    const session = await auth()
-
     return (
         <main>
-            <div>
-                <form action={signIn}>
-                    <Button type="submit">Sign In</Button>
-                </form>
-
-                <form action={signOut}>
-                    <Button type="submit">Sign Out</Button>
-                </form>
-
-                {
-                    session?.user ? <div>{JSON.stringify(session.user)}</div> : <div>Signed out</div>
-                }
-            </div>
+           <h1>Home page</h1>
         </main>
-    );
+    )
 }
